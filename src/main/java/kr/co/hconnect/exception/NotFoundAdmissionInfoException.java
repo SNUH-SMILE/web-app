4ea142ecf8043ee02dc02006a88572c5a53c2b3e
심화용 <hwayonge@hconnect.co.kr>
2021-11-30 16:42:18 +0900

    #2 REST API 개발 NotFoundAdmissionInfoException 주석 누락 수정

--- src/main/java/kr/co/hconnect/exception/NotFoundAdmissionInfoException.java
+++ src/main/java/kr/co/hconnect/exception/NotFoundAdmissionInfoException.java
@@ -2,6 +2,9 @@ package kr.co.hconnect.exception;
 
 import org.springframework.util.StringUtils;
 
+/**
+ * 현재일 기준 내원중인 격리/입소내역이 존재하지 않거나, 한건 이상일 경우 발생
+ */
 public class NotFoundAdmissionInfoException extends RuntimeException {
 
     private static final long serialVersionUID = 7203908342314774373L;
