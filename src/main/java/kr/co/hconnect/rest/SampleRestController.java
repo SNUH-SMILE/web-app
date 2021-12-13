95b6d5cd508c968f724d041f16a5f45a50e31642
이상대 <mousesd@hconnect.co.kr>
2021-12-13 14:02:31 +0900

    #4 REST API 개발환경 설정 - 샘플 API 추가

--- src/main/java/kr/co/hconnect/rest/SampleRestController.java
+++ src/main/java/kr/co/hconnect/rest/SampleRestController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class SampleRestController {
@@ -13,5 +15,10 @@ public class SampleRestController {
     @GetMapping("/api/sample")
     public SampleVO doSample(@Valid @RequestBody SampleVO sampleVO) {
         return sampleVO;
+    }
+
+    @GetMapping("/api/now")
+    public String now() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }
