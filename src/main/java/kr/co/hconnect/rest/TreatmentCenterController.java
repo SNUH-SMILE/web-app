8cf79bbd62b0f39ab342ed999d118ae9fcbad57e
심화용 <hwayonge@hconnect.co.kr>
2022-05-24 14:51:32 +0900

    SMILE-48 생활치료센터 관리  - 생활치료센터 리스트 조회 API 추가

---
+++ src/main/java/kr/co/hconnect/rest/TreatmentCenterController.java
@@ -0,0 +1,38 @@
+package kr.co.hconnect.rest;
+
+import kr.co.hconnect.service.TreatmentCenterService;
+import kr.co.hconnect.vo.TreatmentCenterVO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/treatmentCenter")
+public class TreatmentCenterController {
+
+    /**
+     * 생활치료센터 Service
+     */
+    private final TreatmentCenterService treatmentCenterService;
+
+    /**
+     * 생성자
+     *
+     * @param treatmentCenterService 생활치료센터 Service
+     */
+    public TreatmentCenterController(TreatmentCenterService treatmentCenterService) {
+        this.treatmentCenterService = treatmentCenterService;
+    }
+
+    /**
+     * 생활치료센터 리스트 조회
+     *
+     * @return List&lt;TreatmentCenterVO&gt;
+     */
+    @RequestMapping(value="/list", method = RequestMethod.GET)
+    public List<TreatmentCenterVO> selectTreatmentCenterList() {
+        return treatmentCenterService.selectTreatmentCenterList();
+    }
+}
