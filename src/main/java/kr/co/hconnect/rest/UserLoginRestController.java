6577b2e676f8be7325d853e8c62284ac29130486
심화용 <hwayonge@hconnect.co.kr>
2022-05-24 14:52:05 +0900

    SMILE-41 사용자 로그인

--- src/main/java/kr/co/hconnect/rest/UserLoginRestController.java
+++ src/main/java/kr/co/hconnect/rest/UserLoginRestController.java
@@ -13,11 +13,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class UserLoginRestController {
