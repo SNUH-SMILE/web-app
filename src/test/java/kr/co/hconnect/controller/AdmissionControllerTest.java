425a8921c611554eac31d3650c57a8c0edb00cf2
심화용 <hwayonge@hconnect.co.kr>
2022-06-14 16:55:30 +0900

    SMILE-57 생활치료센터 환자 현황  - 생활치료센터 입소자 퇴소처리 서비스 추가

--- src/test/java/kr/co/hconnect/controller/AdmissionControllerTest.java
+++ src/test/java/kr/co/hconnect/controller/AdmissionControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -38,6 +39,9 @@ public class AdmissionControllerTest {
         mvc = MockMvcBuilders.standaloneSetup(new AdmissionController(admissionService)).build();
     }
 
+    /**
+     * 생활치료센터 입소자 리스트 조회 - 정상확인
+     */
     @Test
     public void selectAdmissionListByCenter_thenSuccess() throws Exception {
         String admissionListSearchByCenterVO
@@ -54,11 +58,91 @@ public class AdmissionControllerTest {
                 "    \"orderDir\":\"DESC\"\n" +
                 "}";
 
-        mvc.perform(post("/api/admission/list/center")
+        mvc.perform(post("/api/admission/center/list")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content(admissionListSearchByCenterVO))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.code", is(ApiResponseCode.SUCCESS.getCode())))
+                .andDo(print());
+    }
+
+    /**
+     * 생활치료센터 입소자 신규 등록 - 정상 확인
+     */
+    @Test
+    public void insertAdmission_thenSuccess() throws Exception {
+        String admissionSaveByCenterVO =
+                "{\n" +
+                "    \"admissionListSearchByCenterVO\":{\n" +
+                "        \"centerId\":\"C001\",\n" +
+                "        \"patientId\":\"\",\n" +
+                "        \"patientNm\":\"\",\n" +
+                "\n" +
+                "        \"currentPageNo\":\"1\",\n" +
+                "        \"recordCountPerPage\":\"15\",\n" +
+                "        \"pageSize\":\"10\",\n" +
+                "        \"totalRecordCount\":\"\",\n" +
+                "        \"orderBy\":\"\",\n" +
+                "        \"orderDir\":\"\"\n" +
+                "    },\n" +
+                "    \"admissionId\":\"\",\n" +
+                "    \"patientId\":\"\",\n" +
+                "    \"patientNm\":\"junitTest\",\n" +
+                "    \"birthDate\":\"19881205\",\n" +
+                "    \"sex\":\"M\",\n" +
+                "    \"cellPhone\":\"01011112222\",\n" +
+                "    \"admissionDate\":\"20220614\",\n" +
+                "    \"dschgeSchdldDate\":\"20220628\",\n" +
+                "    \"personCharge\":\"담당의\",\n" +
+                "    \"centerId\":\"C001\",\n" +
+                "    \"room\":\"01\"\n" +
+                "}";
+
+        mvc.perform(put("/api/admission/center/save")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(admissionSaveByCenterVO))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code", is(ApiResponseCode.SUCCESS.getCode())))
+                .andDo(print());
+    }
+
+    /**
+     * 생활치료센터 입소자 신규 등록 - 이미 존재하는 입소자 확인
+     */
+    @Test
+    public void insertAdmission_thenFailDuplicateActiveAdmission() throws Exception {
+        String admissionSaveByCenterVO =
+                "{\n" +
+                "    \"admissionListSearchByCenterVO\":{\n" +
+                "        \"centerId\":\"C001\",\n" +
+                "        \"patientId\":\"\",\n" +
+                "        \"patientNm\":\"\",\n" +
+                "\n" +
+                "        \"currentPageNo\":\"1\",\n" +
+                "        \"recordCountPerPage\":\"15\",\n" +
+                "        \"pageSize\":\"10\",\n" +
+                "        \"totalRecordCount\":\"\",\n" +
+                "        \"orderBy\":\"\",\n" +
+                "        \"orderDir\":\"\"\n" +
+                "    },\n" +
+                "    \"admissionId\":\"\",\n" +
+                "    \"patientId\":\"\",\n" +
+                "    \"patientNm\":\"junitTest\",\n" +
+                "    \"birthDate\":\"19881205\",\n" +
+                "    \"sex\":\"M\",\n" +
+                "    \"cellPhone\":\"01011112222\",\n" +
+                "    \"admissionDate\":\"20220614\",\n" +
+                "    \"dschgeSchdldDate\":\"20220628\",\n" +
+                "    \"personCharge\":\"담당의\",\n" +
+                "    \"centerId\":\"C001\",\n" +
+                "    \"room\":\"01\"\n" +
+                "}";
+
+        mvc.perform(put("/api/admission/center/save")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(admissionSaveByCenterVO))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code", is(ApiResponseCode.DUPLICATE_PATIENT_INFO.getCode())))
                 .andDo(print());
     }
 
