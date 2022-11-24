065ad4c1bd26da9c41304f44a46ba6ef47b5b21d
baktank71 <44014279+baktank71@users.noreply.github.com>
2022-11-24 17:11:32 +0900

    화상진료 로컬 환경 및 세션 과 토큰 생성 테스트 코드 작성

---
+++ src/main/java/kr/co/hconnect/controller/TeleHealthController.java
@@ -0,0 +1,84 @@
+package kr.co.hconnect.controller;
+
+import kr.co.hconnect.common.ApiResponseCode;
+import kr.co.hconnect.common.VoValidationGroups;
+import kr.co.hconnect.exception.InvalidRequestArgumentException;
+import kr.co.hconnect.jwt.TokenDetailInfo;
+import kr.co.hconnect.vo.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import kr.co.hconnect.service.TeleHealthService;
+import kr.co.hconnect.service.AdmissionService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/teleHealth")
+public class TeleHealthController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TeleHealthController.class);
+
+    private final MessageSource messageSource;
+
+    private final TeleHealthService teleHealthService;
+
+    @Autowired
+    public TeleHealthController(MessageSource messageSource, TeleHealthService teleHealthService) {
+        this.messageSource = messageSource;
+        this.teleHealthService = teleHealthService;
+    }
+
+    @RequestMapping(value = "/getTeleHealth", method = RequestMethod.POST)
+    public ResponseBaseVO<TeleHealthConnectVO> selectConnection(@Validated(VoValidationGroups.add.class) @RequestBody TeleHealthConnectVO vo
+        , BindingResult bindingResult, @RequestAttribute TokenDetailInfo tokenDetailInfo) {
+
+        if (bindingResult.hasErrors()) {
+            throw new InvalidRequestArgumentException(bindingResult);
+        }
+        ResponseBaseVO<TeleHealthConnectVO> responseVO = new ResponseBaseVO<>();
+        try{
+
+            TeleHealthConnectVO dt = teleHealthService.selectConnection(vo);
+            responseVO.setCode(ApiResponseCode.SUCCESS.getCode());
+            responseVO.setMessage("저장 완료");
+            responseVO.setResult(dt);
+
+        }catch (Exception  e){
+            responseVO.setCode(ApiResponseCode.CODE_INVALID_REQUEST_PARAMETER.getCode());
+            responseVO.setMessage(e.getMessage());
+        }
+        return responseVO;
+    }
+    /**
+     *
+     */
+    @RequestMapping(value = "/getSubScriberToken", method = RequestMethod.POST)
+    public ResponseBaseVO<TeleHealthConnectVO> getsubToken(@Validated(VoValidationGroups.add.class) @RequestBody TeleHealthConnectVO vo
+        , BindingResult bindingResult, @RequestAttribute TokenDetailInfo tokenDetailInfo) {
+
+        if (bindingResult.hasErrors()) {
+            throw new InvalidRequestArgumentException(bindingResult);
+        }
+        ResponseBaseVO<TeleHealthConnectVO> responseVO = new ResponseBaseVO<>();
+        try{
+
+            TeleHealthConnectVO dt = teleHealthService.getSubscriberToken(vo);
+            responseVO.setCode(ApiResponseCode.SUCCESS.getCode());
+            responseVO.setMessage("구독토큰발급완료");
+            responseVO.setResult(dt);
+
+        }catch (Exception  e){
+            responseVO.setCode(ApiResponseCode.CODE_INVALID_REQUEST_PARAMETER.getCode());
+            responseVO.setMessage(e.getMessage());
+        }
+        return responseVO;
+    }
+
+}
---
+++ src/main/java/kr/co/hconnect/vo/TeleHealthConnectVO.java
@@ -0,0 +1,36 @@
+package kr.co.hconnect.vo;
+
+import kr.co.hconnect.common.VoValidationGroups;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+/**
+ * 화상상담 연결
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class TeleHealthConnectVO implements Serializable {
+
+    private static final long serialVersionUID = -1932626880281734278L;
+
+    private String loginId;
+
+    private Integer apiKey;                 // API Key
+
+    private String apiSecret;               // Api Secret
+
+    private String sessionId;               // 세션ID
+
+    private String officerToken;            // 담당자 토큰
+
+    private String attendeeToken;           // 참석자 토큰
+
+}
