b7852d126fb0c49baa24ed2137508c969ce960e9
baktank71 <44014279+baktank71@users.noreply.github.com>
2023-01-02 16:14:30 +0900

    배치 우울 테스트 및 수정

--- src/main/java/kr/co/hconnect/batch/BatchController.java
+++ src/main/java/kr/co/hconnect/batch/BatchController.java
@@ -2,12 +2,14 @@ package kr.co.hconnect.batch;
 
 import com.opentok.exception.OpenTokException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import kr.co.hconnect.service.BatchService;
 import org.springframework.stereotype.Controller;
 
 import kr.co.hconnect.vo.*;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 
@@ -15,7 +17,11 @@ import java.io.IOException;
 @Controller
 public class BatchController {
 
-    private String aiPath = "/usr/local/apache-tomcat-8.5.79/python/";
+
+    @Value("${ai.path}")
+    private String ai_path;
+
+    //private String aiPath = "/usr/local/apache-tomcat-8.5.79/python/";
 
     private final BatchService batchService;
 
@@ -34,9 +40,9 @@ public class BatchController {
     @Scheduled(cron="0 0 0/1 * * *")
     public void scoreScheduler() throws IOException, InterruptedException {
 
-        String filePath = aiPath+ "score/score_file.csv";
-        String outfilePath = aiPath + "score/score_result.csv";
-        String executePath = aiPath + "score/scoring.py";
+        String filePath = ai_path+ "score/score_file.csv";
+        String outfilePath = ai_path + "score/score_result.csv";
+        String executePath = ai_path + "score/scoring.py";
 
         //String filePath = "E://python//score//score_file.csv";
         //String outfilePath = "E://python//score//score_result.csv";
@@ -77,9 +83,9 @@ public class BatchController {
     public void bodyTemperatureScheduler() throws IOException, InterruptedException {
         //System.out.println("2. 체온 배치 처리 하기 ");
 
-        String filePath = aiPath+ "temper/temper_file.csv";
-        String outfilePath = aiPath + "temper/temper_result.csv";
-        String executePath = aiPath + "temper/body_temp_rise.py";
+        String filePath = ai_path+ "temper/temper_file.csv";
+        String outfilePath = ai_path + "temper/temper_result.csv";
+        String executePath = ai_path + "temper/body_temp_rise.py";
 
         //String filePath = "E://python//temper//temper_file.csv";
         //String outfilePath = "E://python//temper//temper_result.csv";
@@ -120,9 +126,9 @@ public class BatchController {
     @Scheduled(cron="0 0 23 * * *")
     public void depressedScheduler() throws IOException, InterruptedException {
 
-        String filePath = aiPath+ "depress/depress_file.csv";
-        String outfilePath = aiPath + "depress/depress_result.csv";
-        String executePath = aiPath + "ise.py";
+        String filePath = ai_path+ "depress/annotation.csv";
+        String outfilePath = ai_path + "depress/result.csv";
+        String executePath = ai_path + "ise.py";
 
         //String filePath = "E://python//depress//depress_file.csv";
         //String outfilePath = "E://python//depress//depress_result.csv";
@@ -165,7 +171,13 @@ public class BatchController {
         //파이썬 실행
         //파일 테이블 insert depressedhist / depressedDelete / depressedinsert
 
-        //int rtn = batchService.vonageArchiveList();
+
+        String rtn = batchService.vonageArchiveList();
+
+
+        if (!StringUtils.isEmpty(rtn)){
+
+        }
 
     }
 
