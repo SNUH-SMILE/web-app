9cb6663ec6c5a64d451b1447acd095c0b1f50bf4
baktank71 <44014279+baktank71@users.noreply.github.com>
2023-01-27 13:53:19 +0900

    화상다운 정리

--- src/main/java/kr/co/hconnect/service/TeleHealthService.java
+++ src/main/java/kr/co/hconnect/service/TeleHealthService.java
@@ -84,54 +84,54 @@ public class TeleHealthService extends EgovAbstractServiceImpl {
         BeanUtils.copyProperties(vo, teleVO);
 
         OpenTok openTok = null;
-            try {
-                vo.setApiKey(apikey);
-                vo.setApiSecret(apiSecret);
+        try {
+            vo.setApiKey(apikey);
+            vo.setApiSecret(apiSecret);
 
-                //# 세션 생성
-                openTok = new OpenTok(vo.getApiKey(), vo.getApiSecret());
-                SessionProperties sessionProperties = new SessionProperties.Builder()
-                    .mediaMode(MediaMode.ROUTED)
-                    .build();
-                Session session = openTok.createSession(sessionProperties);
-                String ssid  = session.getSessionId();
-
-
-                String guardianMetaDataFormat = "loginId=%s&admissionId=%s&clientType=pc";
-                String metaData;
-
-                metaData = String.format(guardianMetaDataFormat
-                    , vo.getLoginId()
-                    , ""
-                );
-
-                //화상상담 개설자  토큰생성
-                TokenOptions tokenOptions = new TokenOptions.Builder()
-                    .role(Role.MODERATOR)
-                    .data(metaData)
-                    .build();
-
-                String ofToken = openTok.generateToken(ssid, tokenOptions);
-
-                log.info("화상상담 시작정보 저장");
-
-                //# 화상상담 시작정보 저장
-                teleVO.setLoginId(vo.getLoginId());
-                teleVO.setSessionId(ssid);
-                teleVO.setOfficerToken(ofToken);
-                teleVO.setAdmissionId(vo.getAdmissionId());
-
-                int rtn = teleHealthDao.insertSession(teleVO);
+            //# 세션 생성
+            openTok = new OpenTok(vo.getApiKey(), vo.getApiSecret());
+            SessionProperties sessionProperties = new SessionProperties.Builder()
+                .mediaMode(MediaMode.ROUTED)
+                .build();
+            Session session = openTok.createSession(sessionProperties);
+            String ssid  = session.getSessionId();
 
 
-                //# 참석자(대상자 & 보호자)에게 푸시내역 생성
-                createTelehealthStartPush(teleVO);
+            String guardianMetaDataFormat = "loginId=%s&admissionId=%s&clientType=pc";
+            String metaData;
 
-                openTok.close();
+            metaData = String.format(guardianMetaDataFormat
+                , vo.getLoginId()
+                , ""
+            );
 
-            } catch (OpenTokException e){
-                System.out.println(e.getMessage());
-            }
+            //화상상담 개설자  토큰생성
+            TokenOptions tokenOptions = new TokenOptions.Builder()
+                .role(Role.MODERATOR)
+                .data(metaData)
+                .build();
+
+            String ofToken = openTok.generateToken(ssid, tokenOptions);
+
+            log.info("화상상담 시작정보 저장");
+
+            //# 화상상담 시작정보 저장
+            teleVO.setLoginId(vo.getLoginId());
+            teleVO.setSessionId(ssid);
+            teleVO.setOfficerToken(ofToken);
+            teleVO.setAdmissionId(vo.getAdmissionId());
+
+            int rtn = teleHealthDao.insertSession(teleVO);
+
+
+            //# 참석자(대상자 & 보호자)에게 푸시내역 생성
+            createTelehealthStartPush(teleVO);
+
+            openTok.close();
+
+        } catch (OpenTokException e){
+            System.out.println(e.getMessage());
+        }
         return teleVO;
     }
     @Transactional(rollbackFor = Exception.class)
@@ -281,10 +281,10 @@ public class TeleHealthService extends EgovAbstractServiceImpl {
             String subjectMetaDataFormat = "loginId=%s&admissionId=%s&clientType=mobile";
             String metaData;
             //보호자면
-                metaData = String.format(subjectMetaDataFormat
-                    , ""
-                    , admissionId
-                );
+            metaData = String.format(subjectMetaDataFormat
+                , ""
+                , admissionId
+            );
 
             openTok = new OpenTok(vo.getApiKey(), vo.getApiSecret());
 
@@ -323,204 +323,6 @@ public class TeleHealthService extends EgovAbstractServiceImpl {
     }
 
 
-    /**
-     * 화상 상담 파일 파일 다운로드
-     * @param vo
-     * @return
-     * @throws IOException
-     * @throws OpenTokException
-     * @throws MalformedURLException
-     */
-    public String getTeleArchiveDown(TeleReqArchiveDownVO vo) throws IOException, OpenTokException , MalformedURLException {
-
-        String rtn = "";
-        String metaDataFormat = "화상상담 파일 다운로드가 완료 되었습니다. 경로=%s .";
-        String msg;
-
-        //log.info("ai_local_video_path  >>>  " + ai_local_video_path);
-
-        String outputDir  = "/usr/local/apache-tomcat-8.5.79/python/video/";
-
-        log.info("outputDir  >>>  " + outputDir);
-
-
-        String FILE_URL ="";         // "리소스 경로";
-        //String outputDir  = ai_local_video_path;
-        Archive archive = null;
-
-        InputStream is = null;
-        FileOutputStream os = null;
-
-        LocalDate nowDate = LocalDate.now();
-        LocalTime nowTime = LocalTime.now();
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-        String formatedNow = nowDate.format(formatter);
-
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmmss");
-        String timeFormatedNow = nowTime.format(timeFormatter);
-
-        vo.setCDate(nowDate);
-
-        String archiveId = teleHealthDao.getArchiveId(vo);
-
-        if (StringUtils.isEmpty(archiveId)){
-            return rtn ="31";
-        }
-        String admissionId = vo.getAdmissionId();
-        String patientId = userDao.selectPatientId(admissionId);
-
-        outputDir +=  patientId;
-        log.info("outputDir  >>>  " + outputDir);
-
-        //디렉토리 생성하기
-        File dir = new File(outputDir);
-
-        log.info("dir.exists()  >>>  " + dir.exists());
-
-        if (dir.exists()){
-
-            File newFile = new File(dir+"_"+formatedNow );
-            dir.renameTo(newFile);
-
-            File dirNew = new File(outputDir);
-            dirNew.mkdir();
-
-        } else {
-            try{
-                dir.mkdir();
-                log.info("dir.mkdir() >>>>  폴더가 생성 되었습니다." + dir );
-            } catch (Exception e){
-
-
-                System.out.println(e.getMessage());
-                return "";
-            }
-
-        }
-        //디렉토리 생성하기
-        try{
-
-            OpenTok openTok = new OpenTok(apikey, apiSecret);
-            archive =openTok.getArchive(archiveId);
-
-            FILE_URL = archive.getUrl();
-
-            //rtn = FILE_URL;
-
-            log.info("archive.getUrl  >>>  " + FILE_URL);
-            if (StringUtils.isEmpty(FILE_URL)){
-                log.info("다운로드 할 파일이 없어서 리턴함  " );
-
-                openTok.close();
-
-                return rtn = "31";
-            }
-
-            //해당 url
-            URL url = new URL(FILE_URL);
-            String outputFile = outputDir + "\\" + patientId +".zip";
-
-            log.info(" Archive local Down outputFile >>>>" +  outputFile);
-
-            File f = new File(outputFile);
-
-            log.info(" Archive local Down file create >>>>" + f );
-
-            //파일 다운 로드
-            //FileUtils.copyURLToFile(url, f );  //허가거부
-            //폴더의 문제도 아니고
-
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            int responseCode = conn.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                String fileName = "";
-                String disposition = conn.getHeaderField("Content-Disposition");
-                String contentType = conn.getContentType();
-
-                // 일반적으로 Content-Disposition 헤더에 있지만
-                // 없을 경우 url 에서 추출해 내면 된다.
-                // if (disposition != null) {
-                //     String target = "filename=";
-                //     int index = disposition.indexOf(target);
-                //     if (index != -1) {
-                //         fileName = disposition.substring(index + target.length() + 1);
-                //     }
-                // } else {
-                //     fileName = patientId + ".zip";
-                //
-                // }
-
-                fileName = patientId + ".zip";
-                log.info(" Archive local Down outputDir >>>>" + outputDir);
-                log.info(" Archive local Down fileName >>>>" + fileName);
-
-                is = conn.getInputStream();
-                os = new FileOutputStream(new File(outputDir, fileName));
-
-                log.info(" Archive local getInputStream 시작 >>>>" + is);
-                log.info(" Archive local FileOutputStream 시작 >>>>" + os);
-
-                final int BUFFER_SIZE = 4096;
-                int bytesRead;
-                byte[] buffer = new byte[BUFFER_SIZE];
-
-                log.info(" Archive local Down while 시작 >>>>" );
-
-                while ((bytesRead = is.read(buffer)) != -1) {
-                    os.write(buffer, 0, bytesRead);
-                }
-                os.close();
-                is.close();
-                System.out.println("File downloaded");
-
-            }
-            conn.disconnect();
-            log.info(" Archive local Down file Down load >>>>" );
-
-/*
-            //압축파일 풀기
-            zipUtil ziputil = new zipUtil();
-            String zipFile = patientId + ".zip";
-            //outputDir +="\\"  ;
-            System.out.println("outputDir >> " + outputDir);
-            System.out.println("zipFile >> "+ zipFile);
-
-
-            //압축해제
-            log.error(" Archive local Down 압축해제  >>>>" + zipFile);
-            if(ziputil.unZip(outputDir, zipFile, outputDir)){
-
-                msg= String.format(metaDataFormat
-                    , outputDir
-                );
-
-                rtn = msg;
-            }  //zip 파일 압축해제
-
-            //파일 다운로드 폴더 열기
-            exeProcessbuilder(outputDir);
-*/
-
-            openTok.close();
-        } catch (OpenTokException e) {
-
-            log.error(" Archive local Down OpenTokException >>>>" + e.getMessage());
-
-        // } catch (InterruptedException e) {
-        //     log.error(" Archive local Down InterruptedException >>>>" + e.getMessage());
-        //     throw new RuntimeException(e);
-        }
-/*
-        //정삭적이지
-        if (StringUtils.isEmpty(rtn)){
-            rtn= "31";
-        }
-*/
-        return rtn;
-
-    }
-
     public String getArchiveUrl(TeleReqArchiveDownVO vo) throws IOException, OpenTokException , MalformedURLException {
 
         String rtn = "";
@@ -540,8 +342,7 @@ public class TeleHealthService extends EgovAbstractServiceImpl {
 
     }
 
-
-    /**
+    /**`
      * 푸시발송
      * 화상시작시 고객에게 푸시 발송한다.
      * @param mapValue
@@ -590,15 +391,6 @@ public class TeleHealthService extends EgovAbstractServiceImpl {
             params.put("DB_IN", "Y");
 
 
-
-/*
-            HashMap<String, Object> result = new HttpUtil()
-                .url("http://192.168.42.193:8380/upmc/rcv_register_message.ctl")
-                .method("POST")
-                .body(params)
-                .build();
-*/
-
             HashMap<String, Object> result = new HttpUtil()
                 .url(push_url)
                 .method("POST")
@@ -613,46 +405,6 @@ public class TeleHealthService extends EgovAbstractServiceImpl {
         return rtn;
     }
 
-
-    /**
-     * 파일 탐색기 열기
-     * @return
-     */
-    public String exeProcessbuilder( String arg1) throws IOException, InterruptedException {
-
-        log.info("========파일 탐색기 Processbuilder ================");
-
-        String bool = "";
-        ProcessBuilder builder;
-        BufferedReader br;
-
-        log.info(" 탐색기 실행 프로세스 arg1 >>>> " + arg1);
-
-        try{
-            builder = new ProcessBuilder(ai_local_explorer_path,arg1); //python3 error
-
-            builder.redirectErrorStream(true);
-            Process process = builder.start();
-
-            // 자식 프로세스가 종료될 때까지 기다림
-            int exitval = process.waitFor();
-
-            //// 서브 프로세스가 출력하는 내용을 받기 위해
-            br = new BufferedReader(new InputStreamReader(process.getInputStream(),"UTF-8"));
-
-            log.info("표준출력1  >>>   "+ br );
-
-            String line;
-            while ((line = br.readLine()) != null) {
-                log.info("표준출력 loop  >>>   "+line );
-            }
-        } catch (IOException e){
-            bool = e.getMessage();
-            log.error(e.getMessage());
-        }
-
-        return bool;
-    }
 
 
 }
